cb924c08a69e9dda19e4f8113ad3abe0d7d8655e
Lesego Mabe <lesego@synthesis.co.za>
2025-02-25 10:22:51 +0200

    HHFE-77: Handling null arguments for start transaction, jwt callback, and initialise sdk

--- plugins/flutter/android/src/main/kotlin/za/co/synthesis/halo/sdkflutterplugin/HaloSdkImplementation.kt
+++ plugins/flutter/android/src/main/kotlin/za/co/synthesis/halo/sdkflutterplugin/HaloSdkImplementation.kt
@@ -21,25 +21,27 @@ class HaloSdkImplementation(messanger: BinaryMessenger) {
   fun initializeHaloSDK(result: Result, args: HashMap<String, Any>?) {
     Log.d(TAG, "initializeHaloSDK: $args")
 
-    try {
-      HaloSDK.initialize(
-        HaloInitializationParameters(
-          haloCallbacks,
-          (args!![Const.ON_START_TRANSACTION_TIME_OUT] as Int?)?.toLong() ?: ON_START_TRANSACTION_TIME_OUT,
-          args[Const.APPLICATION_PACKAGE_NAME] as String,
-          args[Const.APPLICATION_VERSION] as String
+    if (args != null) {
+      try {
+        HaloSDK.initialize(
+          HaloInitializationParameters(
+            haloCallbacks,
+            (args[Const.ON_START_TRANSACTION_TIME_OUT] as Int?)?.toLong() ?: ON_START_TRANSACTION_TIME_OUT,
+            args[Const.APPLICATION_PACKAGE_NAME] as String,
+            args[Const.APPLICATION_VERSION] as String
+          )
         )
-      )
-
-      result.success(null)
-    } catch (e: Exception) {
-      if (e is HaloException) {
-        result.error("sdkflutterplugin error", e.message, e.errorCode)
-      } else {
-        result.error("sdkflutterplugin error", e.message, null)
+        result.success(null)
+      } catch (e: Exception) {
+        if (e is HaloException) {
+          result.error("sdkflutterplugin error", e.message, e.errorCode)
+        } else {
+          result.error("sdkflutterplugin error", e.message, null)
+        }
       }
+    } else {
+        result.error("sdkflutterplugin error", "Arguments are null", null)
     }
-
   }
 
   fun cancelTransaction(result: Result) {
@@ -49,35 +51,43 @@ class HaloSdkImplementation(messanger: BinaryMessenger) {
   }
 
   fun startTransaction(result: Result, args: HashMap<String, Any>?) {
-    Log.d(TAG, "startTransaction: $args")
+    if (args != null) {
+      Log.d(TAG, "startTransaction: $args")
 
-    val extraFields = getRest<String>(args as Map<String, Any>, listOf(
-      Const.TRANSACTION_AMOUNT,
-      Const.MERCHANT_TRANSACTION_REFERENCE,
-      Const.TRANSACTION_CURRENCY
-    ))
+      val extraFields = getRest<String>(args as Map<String, Any>, listOf(
+        Const.TRANSACTION_AMOUNT,
+        Const.MERCHANT_TRANSACTION_REFERENCE,
+        Const.TRANSACTION_CURRENCY
+      ))
 
-    try {
-      val startTransactionResult = HaloSDK.startTransaction(
-        (args!![Const.TRANSACTION_AMOUNT] as Double).toBigDecimal(),
-        args[Const.MERCHANT_TRANSACTION_REFERENCE] as String,
-        args[Const.TRANSACTION_CURRENCY] as String,
-        extraFields
-      )
+      try {
+        val startTransactionResult = HaloSDK.startTransaction(
+          (args[Const.TRANSACTION_AMOUNT] as Double).toBigDecimal(),
+          args[Const.MERCHANT_TRANSACTION_REFERENCE] as String,
+          args[Const.TRANSACTION_CURRENCY] as String,
+          extraFields
+        )
 
-      result.success(makeMap(startTransactionResult))
-    } catch (e: Exception) {
-      if (e is HaloException) {
-        result.error("sdkflutterplugin error", e.message, e.errorCode)
-      } else {
-        result.error("sdkflutterplugin error", e.message, null)
+        result.success(makeMap(startTransactionResult))
+      } catch (e: Exception) {
+        if (e is HaloException) {
+          result.error("sdkflutterplugin error", e.message, e.errorCode)
+        } else {
+          result.error("sdkflutterplugin error", e.message, null)
+        }
       }
+    } else {
+      result.error("sdkflutterplugin error", "Arguments are null", null)
     }
   }
 
   fun jwtCallback(result: Result, args: String?) {
-    Log.d(TAG, "jwtCallback: $args")
-    haloCallbacks.jwtCallback(args!!)
-    result.success(null)
+    if (args != null) {
+      Log.d(TAG, "jwtCallback: $args")
+      haloCallbacks.jwtCallback(args)
+      result.success(null)
+    } else {
+      result.error("sdkflutterplugin error", "Arguments are null", null)
+    }
   }
 }
