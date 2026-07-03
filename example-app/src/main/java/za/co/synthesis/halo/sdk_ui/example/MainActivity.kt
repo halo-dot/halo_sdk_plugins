62e4c00ac813888fc457c1c5ba7483b680fc9c7c
Zander <31216542+zcwessels@users.noreply.github.com>
2026-07-03 14:09:59 +0200

    Updated company logo

--- example-app/src/main/java/za/co/synthesis/halo/sdk_ui/example/MainActivity.kt
+++ example-app/src/main/java/za/co/synthesis/halo/sdk_ui/example/MainActivity.kt
@@ -24,6 +24,7 @@ import za.co.synthesis.halo.sdk_ui.models.HDConfig
 import za.co.synthesis.halo.sdk_ui.models.HDSchemeLogos
 import za.co.synthesis.halo.sdk_ui.models.HaloColorScheme
 import za.co.synthesis.halo.sdk_ui.models.HaloTheme
+import java.math.BigDecimal
 import java.security.KeyFactory
 import java.security.spec.PKCS8EncodedKeySpec
 import java.time.Duration
@@ -37,10 +38,10 @@ class MainActivity : ComponentActivity() {
         HaloSdkUi.init(
             HDConfig(
                 this@MainActivity,
-                "za.co.quantumcode.halo_dot",
+                "za.co.synthesis.example",
                 "0.0.1",
-                "Pick n Pay",
-                "logo.png",
+                "PXP",
+                "pxp-logo.png",
                 onTokenRequest = ::getJWTOffline,
                 schemeLogos = HDSchemeLogos(
                     nfc = false,
@@ -52,8 +53,8 @@ class MainActivity : ComponentActivity() {
                 ),
                 theme = HaloTheme(
                     light = HaloColorScheme.default().copy(
-                        primary = Color(0xFFFF0000),
-                        secondary = Color(0xFF2F00FF),
+                        primary = Color(0xFF292CF5),
+                        secondary = Color(0xFF292CF5),
                     )
                 )
             )
@@ -68,8 +69,8 @@ class MainActivity : ComponentActivity() {
                     onClick = {
                         lifecycleScope.launch {
                             val result = HaloSdkUi.launch(
-                                null,
-//                                BigDecimal.valueOf(100.0),
+//                                null,
+                                BigDecimal.valueOf(100.0),
 //                                "Ref passed from the host",
                                 null,
                                 null
